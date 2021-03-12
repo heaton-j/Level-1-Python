b7c8f6ff9cd1bb643a7735d4fea504062bc3292d
heatonj <heatonj@stu.otc.school.nz>
2021-03-12 13:22:59 +1300

    Practise commit completed task 1 completed task 2

---
+++ Task 2.py
@@ -0,0 +1,8 @@
+starting_balance = 50
+print("Balance of gift card: $", starting_balance)
+
+game_price = 35
+print("cost of game: $:", game_price)
+
+remaining_balance = 15
+print("Remaining balance of gift card: $", remaining_balance)
