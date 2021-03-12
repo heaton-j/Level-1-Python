ad7b45a8a48a0c7edaf13c8645857724a37d967b
heatonj <heatonj@stu.otc.school.nz>
2021-03-12 13:11:18 +1300

    Practise commit completed task 1

--- Task.py
+++ Task.py
@@ -1,1 +1,16 @@
-h
+#what scores werer ecorded fo rmatch 1,2, 3, 4, 5
+
+match_1 = 3
+print("first match score: ", match_1)
+match_2 = 6
+print("second match score:", match_2)
+match_3 = 10
+print("third match score:", match_3)
+match_4 = 10
+print("Fourth match score:", match_4)
+match_5 = 6
+print("fifth match score:", match_5)
+
+
+total_points = match_1 + match_2 + match_3 + match_4 + match_5
+print("total points:", total_points)
