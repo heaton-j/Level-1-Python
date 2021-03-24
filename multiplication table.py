c889f24af2d9448d45eacb923135d15526252d72
heatonj <heatonj@stu.otc.school.nz>
2021-03-25 09:18:51 +1300

    finished 1, 2, 3 multiplication table

---
+++ multiplication table.py
@@ -0,0 +1,9 @@
+#mulitplication table up to 12 1, 2, 3
+for i in range(1, 13):
+    print("1 x {} = {}".format(i, i * 1))
+
+for i in range(2,13):
+    print("2 x {} = {}".format(i , i * 2))
+
+for i in range(3, 13):
+    print("3 x {} = {}".format(i, i * 3))
