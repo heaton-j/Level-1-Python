de51f235f8706da202bb6748f42eaaf4dfe491af
heatonj <heatonj@stu.otc.school.nz>
2021-04-13 15:18:07 +1200

    need to finish

---
+++ names.py
@@ -0,0 +1,16 @@
+names = ["Evi", "Madeleine", "Dan", "Kelsey","Cayden", "Hayley", "Darian"]
+names.sort()
+print(names)
+name_check = input("Please enter a name and check if your name is on the list...")
+if name_check in names:
+    print("Your name was on the list!")
+else:
+    print("your name was not on the list")
+name_change = input("Would you like to replace a name with your name?")
+if name_change = "yes":
+    print("What name would you replace?")
+else:
+    name_add = input("Would you like to add a name to the list?")
+    if name_add = "yes":
+        print("")
+
