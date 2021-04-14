27d8e70c32e6cb55881236e101221b42a2f2771a
heatonj <heatonj@stu.otc.school.nz>
2021-04-15 09:39:36 +1200

    need to finish

--- names.py
+++ names.py
@@ -6,11 +6,14 @@ if name_check in names:
     print("Your name was on the list!")
 else:
     print("your name was not on the list")
-name_change = input("Would you like to replace a name with your name?")
-if name_change = "yes":
-    print("What name would you replace?")
+name_change = input("Would you like to replace a name on the list with your name?")
+if name_change == "yes":
+    name_replace = input("What name would you like to replace?")
 else:
-    name_add = input("Would you like to add a name to the list?")
-    if name_add = "yes":
-        print("")
+    print("Okay")
+new = input("Enter a name to replace {}".format(name_replace))
+new_list = names, new
+
+print(new_list)
+
 
